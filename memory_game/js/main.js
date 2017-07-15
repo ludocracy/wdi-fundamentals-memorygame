7787e78a96197db7c32b2dc5ee053de69c5cfcfd
Peter Kong <bansuki@gmail.com>
2017-07-14 19:39:37 -0500

    added functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,23 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-for(var i = 0; i < cardsInPlay.length; i++) {
-	console.log("User flipped " + cardsInPlay[i])
-}
 
-if(cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if(cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again!");
 	}
 }
+
+var flipCard = function(cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+	
+
+	if(cardsInPlay.length === 2) {
+		checkForMatch();
+	}	
+}
+
+flipCard(0);
+flipCard(1);
